9a790f18dd92cde73037da8478984eff58396580
Anthony5321 <tyharpo12@gmail.com>
2023-02-14 10:45:05 -0500

    Got a working delete and sorted my comments.

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -9,15 +9,15 @@ const Review = ({pcId}) => {
     name: '',
   }
   
-  const [oldPost, setOldPost] = useState([])
-  const [newPost, setNewPost] = useState(cleanPost)
+  // const [oldPost, setOldPost] = useState([])
+  const [post, setPost] = useState(cleanPost)
 
       const getPosts = async () => {
         try {
           const res = await axios.get(`http://localhost:3001/api/pc/post/${pcId}`)
-          setOldPost(res.data)
-          console.log(oldPost);
-          // console.log(res)
+          setPost(res.data)
+          console.log(post);
+          console.log(res)
         } catch (err) {
           console.log(err)
         }
@@ -31,9 +31,9 @@ const Review = ({pcId}) => {
       const handleSubmit = async (e) => {
         e.preventDefault()
         try {
-          const createNewPost = {...newPost, pcId: pcId}
+          const createNewPost = {...post, pcId: pcId}
           const response = await axios.post(`http://localhost:3001/api/post`, createNewPost)
-          setNewPost(cleanPost)
+          setPost(cleanPost)
           console.log(response)
           getPosts()
         } catch (error) {
@@ -41,12 +41,19 @@ const Review = ({pcId}) => {
         } 
       }
       const handleChange = (e) => {
-        setNewPost({...newPost, [e.target.name]: e.target.value})
+        setPost({...post, [e.target.name]: e.target.value})
       }
 
 const handleRemove = async (id) => {
-await axios.delete(`http://localhost:3001/api/post/delete/${oldPost}`)
-console.log(oldPost);
+  console.log(post);
+await axios.delete(`http://localhost:3001/api/pc/post/${pcId}/${id}`)
+getPosts()
+}
+
+const handleUpdate =  async (id) => {
+  console.log(post);
+await axios.put(`http://localhost:3001/api/pc/post/${pcId}/${id}`)
+getPosts()
 }
     
 return (
@@ -56,20 +63,19 @@ return (
       <div className="card">
         <h1>Add a comment</h1>
         <form onSubmit={ handleSubmit }>
-        <input type="text" onChange={handleChange} value={newPost.review} name={"review"}  placeholder={'Review'} />
-        <input type="text" onChange={handleChange} value={newPost.rating} name={"rating"} placeholder={'Rating out of 10'} />
-        <input type="text" onChange={handleChange} value={newPost.name} name={"name"} placeholder={'Name'} />
+        <input type="text" onChange={handleChange} value={post.review} name={"review"}  placeholder={'Review'} />
+        <input type="text" onChange={handleChange} value={post.rating} name={"rating"} placeholder={'Rating out of 10'} />
+        <input type="text" onChange={handleChange} value={post.name} name={"name"} placeholder={'Name'} />
         <button>Submit</button>
         </form>
-        {oldPost?.post?.map((old) => (
+        {post?.post?.sort((a,b) => new Date(b.createdAt) - new Date(a.createdAt)).map((old) => (
         <div key={old._id}>
           <h3>Review: {old.review}</h3>
            <p>Rating: {old.rating}</p>
           <p>Name: {old.name}</p>
-          <p>{old.createdAt}</p>
-       <button onClick={handleRemove}>remove</button>
+       <button onClick={() => handleRemove(old._id)}>remove</button>
 
-          </div>
+          </div> 
            ))} 
       </div>
 </div>
